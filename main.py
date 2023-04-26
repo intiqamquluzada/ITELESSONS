38d9adefcc512f8b382d29807e353da54f23e38e
intiqamquluzada <quluzadintiqam@gmail.com>
2023-04-27 01:31:24 +0400

    first commitimm

---
+++ main.py
@@ -0,0 +1,13 @@
+# phrase = ['biz', 'print', 'funksiyasina', 'baxiriq', 've', 'tetbiq', 'edirik']
+#
+#
+# for word in phrase:
+#     print(word, end=' ')
+# print()
+# print()
+# print('salam', 'Intigam', sep='-')
+# print(555)
+
+
+a = 'salam'
+print('hi {} {}'.format(a,a))
