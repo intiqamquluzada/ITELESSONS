4b4cc356333834923a7f9147b6438994dfb06df9
intiqamquluzada <quluzadintiqam@gmail.com>
2023-04-28 00:09:03 +0400

    numeric

--- main.py
+++ main.py
@@ -9,5 +9,36 @@
 # print(555)
 
 
-a = 'salam'
-print('hi {} {}'.format(a,a))
+# a = 'salam'
+# print('hi {} {}'.format(a,a))
+
+
+# a = 2 / 3
+# print(round(a, 3))
+
+
+# a = 1 + 4j
+# print(type(a))
+
+
+# a = 4
+# print(complex(a))
+
+# a = -3
+# print(complex(a))
+
+# a = 4.5
+# print(complex(a))
+
+# a = 5.234547
+# print(round(a, 4))
+
+
+# i = range(6)
+# print(i)
+
+for i in range(5):
+
+    if i == 3:
+        break
+    print(i)
