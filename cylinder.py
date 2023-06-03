9495522dba0a1d01c3b3a5cc8fdfb4fe5ddd4500
intiqamquluzada <quluzadintiqam@gmail.com>
2023-06-03 23:37:39 +0400

    anjod

--- cylinder.py
+++ cylinder.py
@@ -8,3 +8,24 @@ tam_s = lambda r, h: 2*math.pi*r*(r+h)
 
 hecm = lambda r,h: ((math.pi)**2)*h
 
+
+isci = {
+    'Kenan': 1,
+    'Tural': 2,
+    'Yusif': 3,
+}
+
+sirket = ['Buta Group', 'Qmeter', 'MigrationPro']
+
+with open('isciler', 'w', encoding='utf8') as fayl1:
+    fayl1.write("isci = {\n")
+    for key, value in isci.items():
+        fayl1.write(f"    '{key}': '{value}',\n")
+    fayl1.write("}\n\n")
+
+
+with open('sirket', 'w', encoding='utf8') as fayl2:
+    fayl1.write("sirket = {\n")
+    for key, value in isci.items():
+        fayl1.write(f"    '{key}': '{value}',\n")
+    fayl1.write("}\n\n")
